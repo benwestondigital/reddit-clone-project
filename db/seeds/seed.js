26c964ce77af5685a12dda361d97dcf2ffdd0388
Ben <benwestondigital@gmail.com>
2022-01-17 14:04:39 +0000

    change casing for table columns

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -6,12 +6,8 @@ const seed = async data => {
   const { articleData, commentData, topicData, userData } = data;
 
   try {
-    await db.query(`DROP TABLE IF EXISTS comments`);
-    await db.query(`DROP TABLE IF EXISTS articles`);
-    await db.query(`DROP TABLE IF EXISTS users`);
-    await db.query(`DROP TABLE IF EXISTS topics`);
+    await db.query(`DROP TABLE IF EXISTS comments, articles, users, topics`);
 
-    // 1. create tables
     await db.query(`CREATE TABLE topics (
     slug VARCHAR(500) PRIMARY KEY,
     description VARCHAR(500) NOT NULL
@@ -26,11 +22,11 @@ const seed = async data => {
     await db.query(`CREATE TABLE articles (
     article_id SERIAL PRIMARY KEY,
     title VARCHAR(500) NOT NULL,
-    BODY VARCHAR(500) NOT NULL,
-    VOTES INT DEFAULT 0,
-    TOPIC VARCHAR(500) REFERENCES topics(slug),
-    AUTHOR VARCHAR(500) REFERENCES users(username),
-    CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    body VARCHAR(500) NOT NULL,
+    votes INT DEFAULT 0,
+    topic VARCHAR(500) REFERENCES topics(slug),
+    author VARCHAR(500) REFERENCES users(username),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   );`);
 
     await db.query(`CREATE TABLE comments (
@@ -39,7 +35,7 @@ const seed = async data => {
     article_id INT REFERENCES articles(article_id),
     votes INT DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    BODY VARCHAR(500) NOT NULL
+    body VARCHAR(500) NOT NULL
   );`);
   } catch (error) {
     console.log(error);
